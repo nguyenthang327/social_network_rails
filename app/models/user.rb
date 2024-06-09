6e3b48c2ca4fc08e0fef380a9254a07a4ef9a6ef
ThangND <nguyenducthang@Nguyens-MacBook-Pro.local>
2024-06-09 15:37:00 +0700

    feat: user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    before_save :downcase_email
+
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true
+    validates :name, presence: true, length: { minimum: 4, maximum: 30 }
+
+    private
+
+    def downcase_email
+        self.email = email.downcase
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'dashboard/index'
+  # get 'static_pages/home'
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
 end
